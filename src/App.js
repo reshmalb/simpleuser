ee22a7c1e95b038d8c5f138da16f751aad6595f6
reshmalb <reshma.lb27@gmail.com>
2023-03-30 19:26:42 +0530

    error handling

--- src/App.js
+++ src/App.js
@@ -6,22 +6,33 @@ import React,{useState} from 'react';
 function App() {
 const [movies,setMovies]=useState([])
 const [isLoading,setlLoading]=useState(false)
-
+const [isError,setError]=useState(false);
 async function fetchMovieHandler(){
+  setError(null)
   setlLoading(true)
-  const response=await fetch('https://swapi.dev/api/films');
-  const data=await response.json();
-  const transformdata=data.results.map((moviedata)=>{
-    return{
-      id:moviedata.episode_id,
-      title:moviedata.title
+  try{
+           const response=await fetch('https://swapi.dev/api/films');
+               if(!response.ok){
+                     throw new Error('Something went wrong...');
+                  }
+             const data=await response.json();
+  
+ 
+           const transformdata=data.results.map((moviedata)=>{
+          return{
+               id:moviedata.episode_id,
+           title:moviedata.title
 
-    }
-  })
-  setMovies(transformdata)
-  setlLoading(false)
-  console.group(transformdata)
-}
+            }
+        })
+          setMovies(transformdata)
+        console.group(transformdata)
+      }catch(error){
+        setError(error.message);
+        setlLoading(false)
+
+        }
+      }
 
  return(
   <div>
@@ -43,6 +54,7 @@ async function fetchMovieHandler(){
       })}
       {isLoading && <p> Loading...</p>}
       {isLoading && movies.length===0 && <p> No movies found...</p>}
+      {!isLoading && isError&& <p>{isError}</p>}
 
       </div>
     </section>
@@ -50,7 +62,7 @@ async function fetchMovieHandler(){
     
   </div>
  ) 
-
+    
 }
 
 export default App;
