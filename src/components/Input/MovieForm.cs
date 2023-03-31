e1410e45075bf2245eac573f24d1e057c506b140
reshmalb <reshma.lb27@gmail.com>
2023-03-31 10:51:46 +0530

    changes

---
+++ src/components/Input/MovieForm.cs
@@ -0,0 +1,34 @@
+<Card className={classes.form}>
+      <form onSubmit={submitHandler}>
+        <div
+        
+          <label htmlFor="email">E-Mail</label>
+          <input
+            type="email"
+            id="email"
+            value={enteredEmail}
+            onChange={emailChangeHandler}
+            onBlur={validateEmailHandler}
+          />
+        </div>
+        <div
+          className={`${classes.control} ${
+            passwordIsValid === false ? classes.invalid : ''
+          }`}
+        >
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={enteredPassword}
+            onChange={passwordChangeHandler}
+            onBlur={validatePasswordHandler}
+          />
+        </div>
+        <div className={classes.actions}>
+          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+            Login
+          </Button>
+        </div>
+      </form>
+    </Card>
